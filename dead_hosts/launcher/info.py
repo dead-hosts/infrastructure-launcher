a41fc3d6175a54429d32d9ba5a0f3497b196526d
funilrys <contact@funilrys.com>
2020-01-20 23:25:12 +0100

    Happy New Year!

--- dead_hosts/launcher/info.py
+++ dead_hosts/launcher/info.py
@@ -15,8 +15,8 @@ License:
 
     MIT License
 
-    Copyright (c) 2019 Dead Hosts
-    Copyright (c) 2019 Nissar Chababy
+    Copyright (c) 2019, 2020 Dead Hosts
+    Copyright (c) 2019, 2020 Nissar Chababy
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
